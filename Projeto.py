de1cd42789b149a3c01d412a2e770fa7739d00b9
TiagoFar78 <tiagoagostinhofarinha@gmail.com>
2024-10-14 21:04:35 +0100

    improved binary search method

--- Projeto.py
+++ Projeto.py
@@ -1,8 +1,11 @@
 from minizinc import Instance, Model, Solver, Status
 import argparse
 import re
+import time
 from functools import cmp_to_key
 import itertools
+
+SOLVER = "highs"
 
 INPUT_COMMENT = "%"
 INPUT_TESTS_AMOUNT = "Number of tests"
@@ -15,9 +18,12 @@ parser.add_argument('input')
 
 args = parser.parse_args()
 
+######################################################
+#                   Pre-processing                   #
+######################################################
+
 def getId(s):
     return int(s.strip()[2:-1])
-
 
 def getIds(s):
     ids = []
@@ -28,34 +34,18 @@ def getIds(s):
 
     return set(ids)
 
-def fillMachines(machines):
+def fillMachines(machines, n):
     if len(machines) != 0:
         return machines
 
-    for i in range(1, numMachines + 1):
+    for i in range(1, n + 1):
         machines.add(i)
 
     return machines
 
-tests = []
-with open(args.input, 'r') as file:
-    testPattern = re.compile(r"test\( 't(\d+)', (\d+), \[(.*?)\], \[(.*?)\]\)")
-
-    for line in file:
-        if line.startswith(INPUT_COMMENT):
-            value = int(line.split(':')[1].strip())
-            if INPUT_TESTS_AMOUNT in line:
-                numTests = value
-            elif INPUT_MACHINES_AMOUNT in line:
-                numMachines = value
-            elif INPUT_RESOURCES_AMOUNT in line:
-                numResources = value
-        
-        elif line.startswith(INPUT_TEST):
-            match = testPattern.search(line)
-            id, duration, unformattedMachines, unformattedResources = match.groups()
-
-            tests.append((int(duration), fillMachines(getIds(unformattedMachines)), getIds(unformattedResources)))
+######################################################
+#                     Comparator                     #
+######################################################
 
 def comparator(test1, test2):
     resourcesDiff = -(len(test1[2]) - len(test2[2]))
@@ -68,57 +58,47 @@ def comparator(test1, test2):
 
     return -(test1[0] - test2[0])
 
-tests.sort(key=cmp_to_key(comparator))
+######################################################
+#                Lower Bound Estimate                #
+######################################################
 
-durations = []
-machines = []
-resources = []
-
-for test in tests:
-    durations.append(test[0])
-    machines.append(test[1])
-    resources.append(test[2])
-
-def calculatePossibleMakespans(durations):
-    minDuration = max(durations)
-    possibleMakespans = {minDuration}
-
-    for i in range(2, len(durations)):
-        for combination in itertools.combinations(durations, i):
-            currentDuration = sum(combination)
-            if currentDuration > minDuration:
-                possibleMakespans.add(currentDuration)
-
-    uniqueMakespans = list(possibleMakespans)
-    uniqueMakespans.sort()
-    uniqueMakespans.append(sum(durations))
-    return uniqueMakespans
-
-def calculateCombinations(durations):
-    possibleSums = {0}
-
-    for i in range(1, len(durations)):
-        for combination in itertools.combinations(durations, i):
-            possibleSums.add(sum(combination))
+def getMinLowerBound(numResources, tests):
+    higherLowerBound = 0
+    testsDurationSum = 0
+    for r in range(1, numResources + 1):
+        testsDurationSum = 0
+        for test in tests:
+            if r in test[2]:
+                testsDurationSum += test[0]
         
-    return possibleSums
+        if testsDurationSum > higherLowerBound:
+            higherLowerBound = testsDurationSum
+    
+    return higherLowerBound
 
-print("Input:")
-print("Tests: " + str(numTests))
-print("Machines: " + str(numMachines))
-print("Resources: " + str(numResources))
-print("Durations:")
-print(durations)
-print("Machines:")
-print(machines)
-print("Resources:")
-print(resources)
+######################################################
+#                       Models                       #
+######################################################
 
-minizincSolver = Model("./Solver.mzn")
-gecode = Solver.lookup("gecode")
+def isCompleteForLowerbound(solver, model, numTests, numMachines, numResources, durations, machines, resources, lowerBound):
+    instance = Instance(solver, model)
 
-def solveWithLowerBoundVar():
-    instance = Instance(gecode, minizincSolver)
+    instance["nTests"] = numTests
+    instance["nMachines"] = numMachines
+    instance["nResources"] = numResources
+    instance["durations"] = durations
+    instance["machinesAvailable"] = machines
+    instance["resourcesRequired"] = resources
+    instance["lowerBound"] = lowerBound
+
+    result = instance.solve()
+
+    return result.status == Status.SATISFIED
+
+def solveWithLowerBoundVar(numTests, numMachines, numResources, durations, machines, resources, tests):
+    model = Model("./Solver.mzn")
+    solver = Solver.lookup(SOLVER)
+    instance = Instance(solver, model)
 
     instance["nTests"] = numTests
     instance["nMachines"] = numMachines
@@ -132,61 +112,89 @@ def solveWithLowerBoundVar():
 
     print("makespan: " + str(result["lowerBound"]))
 
-
-def solveWithLinearLowerBound():
+def solveWithBinaryLowerBound(numTests, numMachines, numResources, durations, machines, resources, tests):
+    model = Model("./SolverBinary.mzn")
+    solver = Solver.lookup(SOLVER)
     print("O maximo lowerBound é " + str(sum(durations)))
-    for i in range(max(durations), sum(durations) + 1):
-        instance = Instance(gecode, minizincSolver)
-
-        instance["nTests"] = numTests
-        instance["nMachines"] = numMachines
-        instance["nResources"] = numResources
-        instance["durations"] = durations
-        instance["machinesAvailable"] = machines
-        instance["resourcesRequired"] = resources
-        instance["lowerBound"] = i
-
-        print("Vai tentar o lowerbound " + str(i))
-        result = instance.solve()
-
-        if result.status == Status.SATISFIED:
-            print("makespan: " + str(i))
-            break
-
-def solveWithBinaryLowerBound():
-    print("O maximo lowerBound é " + str(sum(durations)))
-    l = max(durations)
+    minLowerBound = getMinLowerBound(numResources, tests)
+    l = minLowerBound
     r = sum(durations)
+    satisfiedLast = False
 
     while l < r - 1:
         mid = int((l + r) / 2)
 
-        instance = Instance(gecode, minizincSolver)
-
-        instance["nTests"] = numTests
-        instance["nMachines"] = numMachines
-        instance["nResources"] = numResources
-        instance["durations"] = durations
-        instance["machinesAvailable"] = machines
-        instance["resourcesRequired"] = resources
-        instance["lowerBound"] = mid
-
         print("Vai tentar o lowerbound " + str(mid))
-        result = instance.solve()
 
-        lastStatus = result.status
-        if result.status == Status.SATISFIED:
-            print("Satisfez: " + str(mid))
+        start_time = time.time()
+        isComplete = isCompleteForLowerbound(solver, model, numTests, numMachines, numResources, durations, machines, resources, mid)
+        elapsed_time = time.time() - start_time
+
+        if isComplete:
+            print("Satisfez: " + str(mid) + f" em: {elapsed_time:.4f}")
             r = mid
+            satisfiedLast = True
         else:
-            print("Não satisfez: " + str(mid))
+            print("Não satisfez: " + str(mid) + f" em: {elapsed_time:.4f}")
             l = mid
+            satisfiedLast = False
 
-    if lastStatus == Status.SATISFIED:
-        makespan = l
-    else:
-        makespan = r
+    if l == minLowerBound:
+        return l if isCompleteForLowerbound(solver, model, numTests, numMachines, numResources, durations, machines, resources, l) else r
 
+    if not satisfiedLast:
+        return l
+
+    return r
+
+######################################################
+#                      Solution                      #
+######################################################
+
+def solve(input):
+    tests = []
+    with open(input, 'r') as file:
+        testPattern = re.compile(r"test\( 't(\d+)', (\d+), \[(.*?)\], \[(.*?)\]\)")
+
+        for line in file:
+            if line.startswith(INPUT_COMMENT):
+                value = int(line.split(':')[1].strip())
+                if INPUT_TESTS_AMOUNT in line:
+                    numTests = value
+                elif INPUT_MACHINES_AMOUNT in line:
+                    numMachines = value
+                elif INPUT_RESOURCES_AMOUNT in line:
+                    numResources = value
+            
+            elif line.startswith(INPUT_TEST):
+                match = testPattern.search(line)
+                id, duration, unformattedMachines, unformattedResources = match.groups()
+
+                tests.append((int(duration), fillMachines(getIds(unformattedMachines), numMachines), getIds(unformattedResources)))
+    
+    tests.sort(key=cmp_to_key(comparator))
+
+    durations = []
+    machines = []
+    resources = []
+
+    for test in tests:
+        durations.append(test[0])
+        machines.append(test[1])
+        resources.append(test[2])
+
+    print("Input:")
+    print("Tests: " + str(numTests))
+    print("Machines: " + str(numMachines))
+    print("Resources: " + str(numResources))
+    print("Durations:")
+    print(durations)
+    print("Machines:")
+    print(machines)
+    print("Resources:")
+    print(resources)
+
+    makespan = solveWithBinaryLowerBound(numTests, numMachines, numResources, durations, machines, resources, tests)
     print("Makespan: " + str(makespan))
 
-solveWithLowerBoundVar()
+solve(args.input)
